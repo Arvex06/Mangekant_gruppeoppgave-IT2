c244bde512876e8d512fdc173df6ad56f42d25c9
Vietnguyen06 <nguyenquocviet2106@gmail.com>
2024-11-05 09:27:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return '<h1>Mangekant generator</h1>'
+@app.route('/success/<name>')
+def success(name):
+    return f'Welcome {name}!'
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form.get('nm')
+    else:
+        user = request.args.get('nm', default="Guest")  # Default to "Guest" if no name is provided
+    
+    if user:
+        return redirect(url_for('success', name=user))
+    else:
+        return "Name is required!", 400  # Bad Request error if name is missing
 
 if __name__ == '__main__':
-
-
-    app.run()
+    app.run(debug=True)
